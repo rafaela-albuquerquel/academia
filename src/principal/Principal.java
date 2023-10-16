7725c28de637da0aa6f30b59664bf39647f1740d
rafaela-albuquerquel <136506049+rafaela-albuquerquel@users.noreply.github.com>
2023-10-16 15:28:04 -0300

    Update Principal.java

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -1,4 +1,29 @@
 package principal;
 
+import cadastro.CadastroDeFuncionarios;
+import cadastro.CadastroDeMembros;
+import aulas.GerenciamentoDeAulas;
+import pagamentos.ControleDePagamentos;
+import cadastro.CadastroDeEquipamentos;
+import cadastro.CadastroDeProdutos;
+import estoque.EstoqueDeProdutos;
+import pagamentos.CompraDeProdutos;
+
 public class Principal {
+
+  public static void main(String[ ] args) {
+
+CadastroDeFuncionarios cadastroDeFuncionarios = new CadastroDeFuncionarios();
+CadastroDeMembros cadastroDeMembros = new CadastroDeMembros();
+GerenciamentoDeAulas gerenciamentoDeAulas = new GerenciamentoDeAulas();
+ControleDePagamentos controleDePagamentos = new ControleDePagamentos();
+CadastroDeEquipamentos cadastroDeEquipamentos = new CadastroDeEquipamentos();
+CadastroDeProdutos cadastroDeProdutos = new CadastroDeProdutos();
+EstoqueDeProdutos estoqueDeProdutos = new EstoqueDeProduto(); 
+CompraDeProdutos compraDeProdutos = new CompraDeProdutos();
+
+
+
+
+  }
 }
